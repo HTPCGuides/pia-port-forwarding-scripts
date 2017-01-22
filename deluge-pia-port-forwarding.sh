e74c62c3b9ed0d3258ff3f42f6de60f85c2bb205
drakehtpc <drakehtpc@users.noreply.github.com>
2017-01-22 18:04:23 +0100

    Update deluge-pia-port-forwarding.sh

--- deluge-pia-port-forwarding.sh
+++ deluge-pia-port-forwarding.sh
@@ -2,6 +2,7 @@
 # Source: http://www.htpcguides.com
 # Adapted from https://github.com/blindpet/piavpn-portforward/
 # Author: Mike
+# Based on https://github.com/crapos/piavpn-portforward
 
 USERNAME=piauser
 PASSWORD=piapass

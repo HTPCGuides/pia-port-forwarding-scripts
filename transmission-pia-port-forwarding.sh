90413833c67ef524c95c55fef803ae4cd76911ed
HTPCGuides <htpcguidez@gmail.com>
2017-05-14 23:33:54 -0400

    Update transmission-pia-port-forwarding.sh
    update author and comment out debug echos

--- transmission-pia-port-forwarding.sh
+++ transmission-pia-port-forwarding.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Source: http://www.htpcguides.com
 # Adapted from https://github.com/blindpet/piavpn-portforward/
-# Author: Mike
+# Author: Mike and Drake
 # Based on https://github.com/crapos/piavpn-portforward
 
 # Set path for root Cron Job
@@ -23,13 +23,13 @@ TRANSHOST=localhost
 
 #get VPNIP
 VPNIP=$(curl -m $CURL_TIMEOUT --interface $VPNINTERFACE "http://ipinfo.io/ip" --silent --stderr -)
-echo $VPNIP
+#echo $VPNIP
 
 #request new port
 PORTFORWARDJSON=$(curl -m $CURL_TIMEOUT --silent --interface $VPNINTERFACE  'https://www.privateinternetaccess.com/vpninfo/port_forward_assignment' -d "user=$USERNAME&pass=$PASSWORD&client_id=$CLIENT_ID&local_ip=$VPNLOCALIP" | head -1)
 #trim VPN forwarded port from JSON
 PORT=$(echo $PORTFORWARDJSON | awk 'BEGIN{r=1;FS="{|:|}"} /port/{r=0; print $3} END{exit r}')
-echo $PORT  
+#echo $PORT  
 
 #change firewall rules if SPLITVPN is set to 1
 if [ "$SPLITVPN" -eq "1" ]; then

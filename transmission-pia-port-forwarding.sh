e40ebb8385f554167c8fea914ca2e4e0da72aad6
George Anton <george.anton.dev@gmail.com>
2017-08-04 01:10:21 +1000

    Replaced PIA API calls with updated API. Previous no longer works. Not Tested with Transmission but should work.

--- transmission-pia-port-forwarding.sh
+++ transmission-pia-port-forwarding.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 # Source: http://www.htpcguides.com
 # Adapted from https://github.com/blindpet/piavpn-portforward/
-# Author: Mike and Drake
+# Author: Mike, Drake and George
 # Based on https://github.com/crapos/piavpn-portforward
 
 # Set path for root Cron Job
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-USERNAME=piauser
-PASSWORD=piapass
 VPNINTERFACE=tun0
-VPNLOCALIP=$(ifconfig $VPNINTERFACE | awk '/inet / {print $2}' | awk 'BEGIN { FS = ":" } {print $(NF)}')
 CURL_TIMEOUT=5
-CLIENT_ID=$(uname -v | sha1sum | awk '{ print $1 }')
+CLIENT_ID=$(head -n 100 /dev/urandom | sha256sum | tr -d " -")
 
 # set to 1 if using VPN Split Tunnel
 SPLITVPN=""
@@ -26,7 +23,7 @@ VPNIP=$(curl -m $CURL_TIMEOUT --interface $VPNINTERFACE "http://ipinfo.io/ip" --
 #echo $VPNIP
 
 #request new port
-PORTFORWARDJSON=$(curl -m $CURL_TIMEOUT --silent --interface $VPNINTERFACE  'https://www.privateinternetaccess.com/vpninfo/port_forward_assignment' -d "user=$USERNAME&pass=$PASSWORD&client_id=$CLIENT_ID&local_ip=$VPNLOCALIP" | head -1)
+PORTFORWARDJSON=$(curl --interface $VPNINTERFACE "http://209.222.18.222:2000/?client_id=$CLIENT_ID" 2>/dev/null)
 #trim VPN forwarded port from JSON
 PORT=$(echo $PORTFORWARDJSON | awk 'BEGIN{r=1;FS="{|:|}"} /port/{r=0; print $3} END{exit r}')
 #echo $PORT  
@@ -42,6 +39,10 @@ if [ "$SPLITVPN" -eq "1" ]; then
         sudo iptables -I INPUT 2 -i $VPNINTERFACE -p tcp --dport $PORT -j ACCEPT
     fi
 fi
+
+#print VPNIP and PORT to text file for easy viewing
+echo $VPNIP > /etc/openvpn/status.txt
+echo $PORT >> /etc/openvpn/status.txt
 
 #change transmission port on the fly
 
